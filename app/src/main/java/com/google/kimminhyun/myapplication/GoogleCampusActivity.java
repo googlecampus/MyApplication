b9d8a74377bc3a9a12b27738c5fe827bb0e9768b
kimminhyun <kimminhyun@google.com>
2015-07-16 20:29:05 +0900

    새로운 Activity를 하나더 추가하였습니다.

---
+++ app/src/main/java/com/google/kimminhyun/myapplication/GoogleCampusActivity.java
@@ -0,0 +1,16 @@
+package com.google.kimminhyun.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kimminhyun on 7/16/15.
+ */
+public class GoogleCampusActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/google/kimminhyun/myapplication/MainActivity.java
+++ app/src/main/java/com/google/kimminhyun/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.google.kimminhyun.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -30,6 +31,8 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, GoogleCampusActivity.class);
+            startActivity(intent);
             return true;
         }
 
