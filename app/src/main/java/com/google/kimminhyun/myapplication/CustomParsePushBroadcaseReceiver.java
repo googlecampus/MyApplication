0266ea700f9dae64df01e6a116e5a65e90065753
kimminhyun <kimminhyun@google.com>
2015-08-06 20:37:43 +0900

    custom parse push receiver

---
+++ app/src/main/java/com/google/kimminhyun/myapplication/CustomParsePushBroadcaseReceiver.java
@@ -0,0 +1,9 @@
+package com.google.kimminhyun.myapplication;
+
+import com.parse.ParseBroadcastReceiver;
+
+/**
+ * Created by kimminhyun on 8/6/15.
+ */
+public class CustomParsePushBroadcaseReceiver extends ParseBroadcastReceiver {
+}
