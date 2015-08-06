31931d51af6f23e58807a6344d023592ebbe1a9b
kimminhyun <kimminhyun@google.com>
2015-08-06 20:51:08 +0900

    푸쉬로 받은 데이터를 토스트로 출력하였습니다.

--- app/src/main/java/com/google/kimminhyun/myapplication/CustomParsePushBroadcaseReceiver.java
+++ app/src/main/java/com/google/kimminhyun/myapplication/CustomParsePushBroadcaseReceiver.java
@@ -1,9 +1,26 @@
 package com.google.kimminhyun.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 import com.parse.ParseBroadcastReceiver;
 
 /**
+ * Custom receiver for customizing when push received.
+ *
  * Created by kimminhyun on 8/6/15.
  */
 public class CustomParsePushBroadcaseReceiver extends ParseBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getExtras() != null) {
+            Toast.makeText(context, "intent extra : " + intent.getExtras().toString(), Toast.LENGTH_LONG).show();
+        }
+        if (intent.getDataString() != null) {
+            Toast.makeText(context, "intent data : " + intent.getDataString(), Toast.LENGTH_LONG).show();
+        }
+    }
 }
